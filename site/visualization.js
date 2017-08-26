f1adc417cbf21d32307a87edea86dff9babeb107
Max von Hippel <maxvonhippel1996@gmail.com>
2017-08-25 19:24:35 -0700

    JS cleanup

--- site/visualization.js
+++ site/visualization.js
@@ -10,21 +10,24 @@ Plotly.d3.csv('../stars.csv', function(err, rows) {
     z:unpack(rows, 'w'),
     mode: 'markers',
     marker: {
-    size: d3.interpolateRdYlBu(unpack(rows, 't')/13),
-    line: {
       color: 'rgba(217, 217, 217, 0.14)',
-      width: 0.5
+      size: d3.interpolateRdYlBu(unpack(rows, 't')/13),
+      line: {
+        color: 'rgba(217, 217, 217, 0.14)',
+        width: 0.5
+      },
+      opacity: 0.8
     },
-    opacity: 0.8
-  },
-  type: 'scatter3d'
-}];
-var layout = {margin: {
-    l: 0,
-    r: 0,
-    b: 0,
-    t: 0
-  }};
-Plotly.newPlot('chart', data, layout);
+    type: 'scatter3d'
+  }];
+  var layout = {
+    margin: {
+      l: 0,
+      r: 0,
+      b: 0,
+      t: 0
+    }
+  };
+  Plotly.newPlot('chart', data, layout);
 });
 console.log('example of ', (11/13), d3.interpolateRdYlBu(11/13));
