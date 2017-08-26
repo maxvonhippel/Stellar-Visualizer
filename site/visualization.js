ed7924b084c85752e4e522eec65bdc45d73aa88c
Max von Hippel <maxvonhippel1996@gmail.com>
2017-08-25 20:09:26 -0700

    Added nice formatting

--- site/visualization.js
+++ site/visualization.js
@@ -13,13 +13,23 @@ Plotly.d3.csv('../stars.csv', function(err, rows) {
       cmin: 0,
       cmax: 13,
       color: unpack(rows, 't'),
-      colorscale: 'Electric',
+      colorscale: 'Rainbow',
       size: 4,
       opacity: 0.8
     },
     type: 'scatter3d'
   }];
   var layout = {
+    title: 'Locations & Ages of White Dwarfs',
+    xaxis: {
+      title: 'U'
+    },
+    yaxis: {
+      title: 'V'
+    },
+    zaxis: {
+      title: 'W'
+    },
     margin: {
       l: 0,
       r: 0,
