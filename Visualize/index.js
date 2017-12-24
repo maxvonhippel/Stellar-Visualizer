b403c2d4733562b5f9f379c2b048da389ab01242
Max von Hippel <maxvonhippel1996@gmail.com>
2017-12-23 23:37:40 -0900

    First pass implementation of equatorial_to_uvw

--- Visualize/index.js
+++ Visualize/index.js
@@ -3,63 +3,38 @@ var data = null;
 var graph = null;
 
 // Called when the Visualization API is loaded.
-function drawVisualization() {
-	// create the data table.
-	// data = new vis.DataSet();
-	var csv_file = "http://mxvh.pl/Stellar-Visualizer/Visualize/data.csv";
-	var csv_data = Papa.parse(csv_file, {
+function drawVisualization(csv_file) {
+	console.log(csv_file.name);
+	var pow = Math.pow;
+	// parse the csv
+	var data = new vis.DataSet();
+	var lines = Papa.parse(csv_file, {
+		download: true,
 		header: true,
-		step: function(line) {
-			// line = i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t, u, v, w
-			var new_line = {
-				x:line[8],
-				y:line[9],
-				z:line[10],
-				style: Math.sqrt(Math.pow(line[1], 2) +
-					   Math.pow(line[2], 2) + 
-					   Math.pow(line[3], 2)),
-				id: line[0]
-			};
-			console.log("BANANA BANANA BANANA");
-			console.log(new_line);
-		// data.add(new_line);
+		step: function(row) {
+			var row_data = row.data[0];
+			console.log("ROW.i ", row_data.i);
+			data.add({id:row_data.i,
+					  x:row_data.u,
+					  y:row_data.v,
+					  z:row_data.w,
+					  style:"#00ffff"
+					});
+		},
+		complete: function() {
+			console.log("DONE");
 		}
 	});
-
-	// // create some shortcuts to math functions
-	// var sqrt = Math.sqrt;
-	// var pow = Math.pow;
-	// var random = Math.random;
-
-	// // create the animation data
-	// var imax = 100;
-	// for (var i = 0; i < imax; i++) {
-	// 	var x = pow(random(), 2);
-	// 	var y = pow(random(), 2);
-	// 	var z = pow(random(), 2);
-	// 	var style = (i%2==0) ? sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) : "#00ffff";
-
-	// 	data.add({x:x,y:y,z:z,style:style});
-	// }
-
-	// specify options
-	var options = {
-		width:  '600px',
-		height: '600px',
-		style: 'dot-color',
-		showPerspective: true,
-		showGrid: true,
-		keepAspectRatio: true,
-		verticalRatio: 1.0,
-		legendLabel: 'distance',
-		cameraPosition: {
-		  horizontal: -0.35,
-		  vertical: 0.22,
-		  distance: 1.8
-		}
-	};
-
-	// create our graph
-	// var container = document.getElementById('mygraph');
-	// graph = new vis.Graph3d(container, data, options);
+    var options = {
+        width:  '500px',
+        height: '552px',
+        style: 'surface',
+        showPerspective: true,
+        showGrid: true,
+        showShadow: false,
+        keepAspectRatio: true,
+        verticalRatio: 0.5
+    };
+    var container = document.getElementById('visualization');
+    var graph3d = new vis.Graph3d(container, data, options);
 }
---
+++ generate_synthetic_data.py
@@ -0,0 +1,168 @@
+# generate_synthetic_data.py
+# 
+# Generates synthetic data files which are used to prototype a
+# data visualization system.  The synthetic files are rough
+# estimates of the sort of data which we will get from Gaia.
+#
+# Authored August 24 2017 by Max von Hippel
+# Last edited December 22 2017 by Max & Ted von Hippel
+
+import random
+import csv
+import uuid
+import numpy as np
+
+# ------------------------- Assumptions -------------------------
+
+# These are the hypothetical parameters of the distributions
+# of the data (See generate_uvw).
+thin_disk  = {'u': [0,     20], \
+			  'v': [0,     20], \
+			  'w': [0,     10], \
+			  't': [0,     10]}
+thick_disk = {'u': [0,     30], \
+			  'v': [-20,   30], \
+			  'w': [0,     30], \
+			  't': [9,     11]}
+halo 	   = {'u': [0,     50], \
+			  'v': [-200, 100], \
+			  'w': [0,     60], \
+			  't': [11,    13]}
+
+# These are the hypothetical populations of the data.
+populations = [thin_disk, thick_disk, halo]
+
+# ------------------------- Methods -------------------------
+
+# uvw_to_equatorial
+# ---------------------------------------------------
+# Transforms a star in uvw to equatorial, filling in
+# unknown values when necesarry based on assumptions
+# explained in the code.  (Not entirely deterministic.)
+#
+# Input:
+# 	star = (i, u, v, w, t)
+# Output:
+#	star = i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t)
+def uvw_to_equatorial(star):
+	# input star is 5-tuple of form (i, u, v, w, t)
+	# Right Ascension, the longitude-like coordinate on the celestial sphere
+	# RA is uniform [0, 360] with modulo 360
+	RA = random.uniform(0,360)
+	RA = RA if RA != 360 else 0
+	# Declination, the latitude-like coordinate on the celestial sphere
+	# Dec is uniform [-90, 90]
+	Dec = random.uniform(-90,90)
+	# From u, v, w we will get:
+	# pm_RA
+	# pm_Dec
+	# Parallax in arcsec.  This is a nuisance variable for this visualization,
+	# therefore we generate an arbitrary/nominal value here.
+	prlx = 100
+	# V_rad
+	# We will return the following 8-tuple:
+	# (i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t)
+
+# equatorial_to_uvw
+# ---------------------------------------------------
+# Transforms a star in equatorial to uvw, filling in
+# unknown values when necesarry based on assumptions
+# explained in the code.  (Not entirely deterministic.)
+#
+# Reference: http://adsabs.harvard.edu/full/1987AJ.....93..864J
+#
+# Input:
+# 	star = (i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t)
+# Output:
+# star = (i, u, v, w, t)
+def equatorial_to_uvw(star):
+	(i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t) = star
+	k = 4.74057
+	T = np.matrix([-0.06699 -0.87276 -0.48354], 
+				  [0.49273 -0.45035 0.74458],
+				  [-0.86760 -0.18837 0.46020])
+	A = np.matrix([cos(RA) * cos(Dec) -sin(RA) -cos(RA) * sin(Dec)],
+				  [sin(RA) * cos(Dec) cos(RA) -sin(RA) * sin(Dec)],
+				  [sin(Dec) 0 cos(Dec)])
+	B = T * A
+	(u, v, w) = B * np.matrix([RV],
+							  [k * pm_RA / prlx],
+							  [k * pm_Dec / prlx])
+	return (i, u, v, w, t)
+
+# generate_uvw
+# ---------------------------------------------------
+# Generates a random star in uvw based on rough assumptions
+# regarding general distribution of the galaxy.
+# See Assumptions section of code near top for more clarity
+# RE how this method works.
+#
+# Input:
+# 	type = thin_disk, thick_disk, or halo
+# Output:
+#	star = (i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t)
+def generate_uvw(type):
+	# u - UUID.  Simulates the steller id in the Gaia data.
+	i = uuid.uuid4()
+	u = random.gauss(type['u'][0], type['u'][1])
+	v = random.gauss(type['v'][0], type['v'][1])
+	w = random.gauss(type['w'][0], type['w'][1])
+	# t - Stellar 
+	t = random.uniform(type['t'][0], type['t'][1])
+	return i, u, v, w, t
+
+# write_lines
+# ---------------------------------------------------
+# Writes a long list of generated stars in CSV format to
+# a file, which is the input to the visualization code.
+# Formatted as follows:
+# i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t, u, v, w
+#
+# Input:
+# 	stars 	 - a list of stars.  Each star is a fairly complicated
+#			   tuple of the form:
+#			   (i, RA, Dec, pm_RA, pm_Dec, prlx, V_rad, t, u, v, w)
+#			   Essentially, a star is a concatenation of uvw and
+#			   equatorial variables, because we might as well
+#			   include everything in our CSV pre-processed so we can
+#			   visualize whatever we want on-the-fly without any hefty
+#			   intermediary computation.
+#	filename - the filepath to write to.  Either absolute (begin with ~)
+#			   or relative to this file (generate_synthetic_data.py) 
+#			   should work.
+# Output:
+#	success  - True if file was successfully written, else Error object.
+def write_lines(stars, filename):
+	# TODO: add a try/catch, if error return the error.
+	with open(filename, 'w') as file:
+		out = csv.writer(file)
+		# Write the header of the CSV file
+		out.writerow(['i', 'RA', 'Dec', 'pm_RA', 'pm_Dec', 'prlx', 'V_rad', \
+					  't', 'u', 'v', 'w'])
+		for star in stars:
+			out.writerow(star)
+	# Presumably we are successful if we make it this far.
+	return True
+
+# generate_n_data
+# ---------------------------------------------------
+# Generates n-many synthetic stars and writes all the data
+# to a file.
+#
+# Input:
+# 	n 		 - how many synthetic stars to generate.
+#	filename - the filepath to write to.  Either absolute (begin with ~)
+#			   or relative to this file (generate_synthetic_data.py) 
+#			   should work.
+# Output:
+#	success  - True if file was successfully written, else Error object.
+def generate_n_data(n, filename):
+	# TODO: add a try/catch, if error return the error.
+	stars = []
+	for i in range(n):
+		population = random.choice(populations)
+		star = makeStar(population)
+		stars.append(star)
+	writeLines(stars, filename)
+	# Presumably we are successful if we make it this far.
+	return True
