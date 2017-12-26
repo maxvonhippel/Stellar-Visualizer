2de96acab96be55c5a5a9ba1bb89d6cd4f2bd3d6
Max von Hippel <maxvonhippel1996@gmail.com>
2017-12-26 15:19:08 -0700

    Axis switching now working perfectly.  Need to talk to ted about what axes we want to support, or if we should support arbitrary axis selection, but the underlying infrastructure is there now

--- Visualize/index.js
+++ Visualize/index.js
@@ -1,27 +1,34 @@
 // Initialize local variables to null
 var data = null;
 var graph3d = null;
+var full_data = {};
+var container = null;
+var options = 
+{
+    width:  '600px',
+    height: '600px',
+    style: 'dot-color',
+    showPerspective: true,
+    showGrid: true,
+    showShadow: false,
+    keepAspectRatio: true,
+    verticalRatio: 0.5
+};
+var UVW = true;
 
 // Called when the Visualization API is loaded.
-function drawVisualization(csv_file) {
+function draw_visualization(csv_file) 
+{
 	var pow = Math.pow;
 	var sqrt = Math.sqrt;
 	// parse the csv
 	data = new vis.DataSet();
-	var options = {
-        width:  '600px',
-        height: '600px',
-        style: 'dot-color',
-        showPerspective: true,
-        showGrid: true,
-        showShadow: false,
-        keepAspectRatio: true,
-        verticalRatio: 0.5
-    };
-	var lines = Papa.parse(csv_file, {
+	var lines = Papa.parse(csv_file, 
+	{
 		download: true,
 		header: true,
-		step: function(row) {
+		step: function(row) 
+		{
 			var row_data = row.data[0];
 			var id = parseInt(row_data.i);
 			var x  = parseInt(row_data.u);
@@ -34,20 +41,86 @@ function drawVisualization(csv_file) {
 			style = parseInt(style);
 			var data_row = {id:id, x:x, y:y, z:z, style:style};
 			data.add(data_row);
+			// i,RA,Dec,pm_RA,pm_Dec,prlx,V_rad,t,u,v,w
+			full_data[id] = 
+			{
+				RA:parseInt(row_data.RA),
+				Dec:parseInt(row_data.Dec),
+				pm_RA:parseInt(row_data.pm_RA),
+				pm_Dec:parseInt(row_data.pm_Dec),
+				prlx:parseInt(row_data.prlx),
+				V_rad:parseInt(row_data.V_rad),
+				t:parseInt(row_data.t),
+				u:x,
+				v:y,
+				w:z
+			};
 		},
-		complete: function() { 
-			var container = document.getElementById('visualization');
+		complete: function() 
+		{ 
+			container = document.getElementById('visualization');
     		graph3d = new vis.Graph3d(container, data, options); 
     		console.log('done drawing');
     	}
 	});
 }
 
-function handleFileSelect(evt) {
+function handle_file_select(evt) 
+{
   	var files = evt.target.files;
     // only select first one
-    drawVisualization(files[0]);
+    draw_visualization(files[0]);
 }
-document.getElementById('file').addEventListener('change', 
-  												 handleFileSelect, 
-  												 false);
+document.getElementById('file')
+		.addEventListener('change', handle_file_select, false);
+
+// ------------ TODO ------------------
+/* 1. Need to make it so data contains all data not just x, y, z
+ * 2. Would be great to make this more elegant, so that maybe we have 1
+ * dataset for all the data and a second for just the x, y, z or something?
+ * (Is that scaleable???)  Then we could just change the second based on the
+ * first.
+ * 3. Then comes all the UI stuff like showing a buffering indicator of some
+ * sort, including axis labels, etc.
+ */
+
+function change_coordinates() 
+{
+	// i,RA,Dec,pm_RA,pm_Dec,prlx,V_rad,t,u,v,w
+	if (data == null || data._data == null) return;
+	UVW = !UVW;
+	for (var line in data._data) {
+		var cur_line = data._data[line];
+		var cur_line_data = full_data[line];
+		if (UVW == true)
+		{
+			cur_line.x = cur_line_data.u;
+			cur_line.y = cur_line_data.v;
+			cur_line.z = cur_line_data.w;
+		}
+		else
+		{
+			cur_line.x = cur_line_data.RA;
+			cur_line.y = cur_line_data.Dec;
+			cur_line.z = cur_line_data.V_rad;
+		}
+	}
+	graph3d = new vis.Graph3d(container, data, options); 
+	// if (this.selectedIndex == 0)
+	// {
+	// 	// UVW
+	// 	console.log('Selected (U, V, W). \
+	// 				 Sadly, axis switching is not done yet.');
+	// 	console.log(data._data);
+
+	// } 
+	// else 
+	// {
+	// 	// Ecuatorial
+	// 	console.log('Selected Ecuatorial. \
+	// 				 Sadly, axis switching is not done yet.');
+	// 	console.log(data._data);
+	// }
+}
+document.getElementById('coordinatePicker')
+		.addEventListener('change', change_coordinates);
