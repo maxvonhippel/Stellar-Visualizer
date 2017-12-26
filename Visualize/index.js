7cb84c63c2ba7662e527209835890a80a944450b
Max von Hippel <maxvonhippel1996@gmail.com>
2017-12-26 15:49:59 -0700

    Completely done with rudimental axis switching, also labels on axes and on legend.  Next need to improve UI intuititive-ness.  (Which is not a word

--- Visualize/index.js
+++ Visualize/index.js
@@ -1,7 +1,6 @@
 // Initialize local variables to null
 var data = null;
 var graph3d = null;
-var full_data = {};
 var container = null;
 var options = 
 {
@@ -34,32 +33,33 @@ function draw_visualization(csv_file)
 			var x  = parseInt(row_data.u);
 			var y  = parseInt(row_data.v);
 			var z  = parseInt(row_data.w);
-			// Can change style selection later to be more directly informative
-			// (a 4th dimension of some sort, maybe galactic age or something).
-			var style = (id % 2 == 0) ? 
-						sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)) : "#00ffff";
-			style = parseInt(style);
-			var data_row = {id:id, x:x, y:y, z:z, style:style};
-			data.add(data_row);
-			// i,RA,Dec,pm_RA,pm_Dec,prlx,V_rad,t,u,v,w
-			full_data[id] = 
-			{
+			var t = parseInt(row_data.t);	// use t for style
+			data.add({
+				id:id,
 				RA:parseInt(row_data.RA),
 				Dec:parseInt(row_data.Dec),
 				pm_RA:parseInt(row_data.pm_RA),
 				pm_Dec:parseInt(row_data.pm_Dec),
 				prlx:parseInt(row_data.prlx),
 				V_rad:parseInt(row_data.V_rad),
-				t:parseInt(row_data.t),
+				t:t,
 				u:x,
 				v:y,
-				w:z
-			};
+				w:z,
+				x:x,
+				y:y,
+				z:z,
+				style:parseInt(t),
+			});
 		},
 		complete: function() 
 		{ 
 			container = document.getElementById('visualization');
     		graph3d = new vis.Graph3d(container, data, options); 
+   			set_graph_options();
+   			// these options *should* only need to be set once
+   			graph3d.tooltip = true;
+			graph3d.legendLabel = "Galactic Age";
     		console.log('done drawing');
     	}
 	});
@@ -71,8 +71,11 @@ function handle_file_select(evt)
     // only select first one
     draw_visualization(files[0]);
 }
+
 document.getElementById('file')
-		.addEventListener('change', handle_file_select, false);
+		.addEventListener('change',
+						  handle_file_select,
+						  false);
 
 // ------------ TODO ------------------
 /* 1. Need to make it so data contains all data not just x, y, z
@@ -84,43 +87,37 @@ document.getElementById('file')
  * sort, including axis labels, etc.
  */
 
+function set_graph_options()
+{
+	graph3d.xLabel = UVW ? "u" : "RA";
+	graph3d.yLabel = UVW ? "v" : "Dec";
+	graph3d.zLabel = UVW ? "w" : "V_rad";
+}
+
 function change_coordinates() 
 {
-	// i,RA,Dec,pm_RA,pm_Dec,prlx,V_rad,t,u,v,w
 	if (data == null || data._data == null) return;
 	UVW = !UVW;
 	for (var line in data._data) {
 		var cur_line = data._data[line];
-		var cur_line_data = full_data[line];
 		if (UVW == true)
 		{
-			cur_line.x = cur_line_data.u;
-			cur_line.y = cur_line_data.v;
-			cur_line.z = cur_line_data.w;
+			cur_line.x = cur_line.u;
+			cur_line.y = cur_line.v;
+			cur_line.z = cur_line.w;
 		}
 		else
 		{
-			cur_line.x = cur_line_data.RA;
-			cur_line.y = cur_line_data.Dec;
-			cur_line.z = cur_line_data.V_rad;
+			cur_line.x = cur_line.RA;
+			cur_line.y = cur_line.Dec;
+			cur_line.z = cur_line.V_rad;
 		}
 	}
-	graph3d = new vis.Graph3d(container, data, options); 
-	// if (this.selectedIndex == 0)
-	// {
-	// 	// UVW
-	// 	console.log('Selected (U, V, W). \
-	// 				 Sadly, axis switching is not done yet.');
-	// 	console.log(data._data);
-
-	// } 
-	// else 
-	// {
-	// 	// Ecuatorial
-	// 	console.log('Selected Ecuatorial. \
-	// 				 Sadly, axis switching is not done yet.');
-	// 	console.log(data._data);
-	// }
+	graph3d.setData(data);
+	set_graph_options();
+	graph3d.redraw();
 }
+
 document.getElementById('coordinatePicker')
-		.addEventListener('change', change_coordinates);
+		.addEventListener('change',
+						  change_coordinates);
